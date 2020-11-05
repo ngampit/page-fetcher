f338693b505d0cef470ae0722e252b94b1878b1d
@mollysutthasilp <ngampitt@yahoo.com>
2020-11-05 05:38:46 +0000

    clean code

--- fetcher.js
+++ fetcher.js
@@ -4,19 +4,12 @@ const args = process.argv;
 
 let url = args[2];
 let file = args[3];
-//console.log(url)
-//console.log(file)
-
-// request(url, (error,response,body) => {
-//     console.log('body',body);
-// });
 
 
-//const request = require('request');
 request(url, (error, response, body) => {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
     fs.writeFile(file, body, function (err) {
         if (err) return console.log("write file err");
         console.log('Downloaded and saved 3261 bytes to ./index.html');
